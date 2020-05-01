43ca8b97b4dfb3aa8a082f89f08a10110721bae7
Roberto Cortez <radcortez@yahoo.com>
2020-05-01 15:42:20 +0100

    Implemented new method in ConfigSource from MP 2.0.

--- implementation/src/test/java/io/smallrye/config/ConfigSourceWrapperTestCase.java
+++ implementation/src/test/java/io/smallrye/config/ConfigSourceWrapperTestCase.java
@@ -24,6 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigSource;
@@ -96,6 +97,11 @@ public class ConfigSourceWrapperTestCase {
 
         public Map<String, String> getProperties() {
             return delegate.getProperties();
+        }
+
+        @Override
+        public Set<String> getPropertyNames() {
+            return delegate.getPropertyNames();
         }
 
         public String getValue(final String propertyName) {
